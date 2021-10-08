1d24c031e5796a8cf475b060a901e0ba7756ca25
Blavkjay <balogunjuwon123@gmail.com>
2021-10-08 22:55:30 +0100

    create repo search view model

---
+++ IntentTakeHomeTest/Search/presentation/viewModel/RepoSearchViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  RepoSearchViewModel.swift
+//  IntentTakeHomeTest
+//
+//  Created by Admin on 08/10/2021.
+//
+
+import Foundation
+
+
+public class RepoSearchViewModel {
+    
+    let repoSearchLoader: RepoSearchLoader
+    
+    init(repoSearchLoader: RepoSearchLoader){
+        self.repoSearchLoader = repoSearchLoader
+    }
+    
+    var onload: (() -> Void)?
+    var onSuccess: ((RepoListResponse) -> Void)?
+    var onError: ((String) -> Void)?
+    
+    func searchGit(queryString: String) {
+        onload?()
+        
+        repoSearchLoader.getRepositoriesFromSearch(queryString: queryString, page: 1, perPageNumber: 20) { [weak self] result in
+            switch result {
+            case let .success(model):
+                self?.onSuccess?(model)
+            case let .failure(error):
+                self?.onError?(error.message)
+            }
+        }
+    }
+    
+}
